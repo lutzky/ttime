12f6b980b8ef273824b6425ca56a11d10339a44c
haggai <haggai@cdcc5ea1-6e19-0410-9661-ca766f925b7c>
2006-08-02 06:35:14 +0000

    treeview example with a custom renderer
    git-svn-id: file:///svn/ttime@49 cdcc5ea1-6e19-0410-9661-ca766f925b7c

--- Gtk/TreeViewExample.cs
+++ Gtk/TreeViewExample.cs
@@ -1,4 +1,36 @@
 using Gtk;
+using Gdk;
+using System;
+
+public class CR : CellRenderer
+{
+	protected CellRendererText txt;
+	public CR() : base()
+	{
+		txt = new CellRendererText();
+	}
+
+	protected override void Render(Gdk.Drawable win, Widget widget, Gdk.Rectangle bg_area, Gdk.Rectangle cell_area, Gdk.Rectangle expose_area,
+			CellRendererState flags)
+	{
+		Gdk.GC gc = new Gdk.GC(win);
+		Gdk.Color red_color = new Gdk.Color(0xff, 0, 0);
+	       	Gdk.Colormap.System.AllocColor(ref red_color, true, true);	
+		gc.Foreground = red_color;
+		win.DrawRectangle(gc, true, bg_area);
+		txt.Render((Gdk.Window)win, widget, bg_area, cell_area, expose_area, flags);
+	}
+
+	public override void GetSize(Widget widget, ref Rectangle cell_area, out int x_offset, out int y_offset, out int width, out int height)
+
+	{
+		txt.GetSize(widget, ref cell_area, out x_offset, out y_offset, out width, out height);
+	}
+
+	public string Text { get { return txt.Text; }
+		set { txt.Text = value; }
+	}
+}
 
 public class TreeViewExample
 {
@@ -27,7 +59,7 @@ public class TreeViewExample
 		artistColumn.Title = "Artist";
  
 		// Create the text cell that will display the artist name
-		Gtk.CellRendererText artistNameCell = new Gtk.CellRendererText ();
+		CR artistNameCell = new CR();
 		artistNameCell.Height = 80;
 		artistNameCell.Width = 80;
 		
@@ -39,7 +71,7 @@ public class TreeViewExample
 		songColumn.Title = "Song Title";
  
 		// Do the same for the song title column
-		Gtk.CellRendererText songTitleCell = new Gtk.CellRendererText ();
+		CR songTitleCell = new CR();
 		songTitleCell.Height = 80;
 		songTitleCell.Width = 80;
 		songColumn.PackStart (songTitleCell, true);
@@ -49,8 +81,8 @@ public class TreeViewExample
 		tree.AppendColumn (songColumn);
  
 		// Tell the Cell Renderers which items in the model to display
-		artistColumn.AddAttribute (artistNameCell, "text", 0);
-		songColumn.AddAttribute (songTitleCell, "text", 1);
+		artistColumn.SetCellDataFunc(artistNameCell, new TreeCellDataFunc(f1));
+		songColumn.SetCellDataFunc(songTitleCell, new TreeCellDataFunc(f2));
  
 		// Create a model that will hold two strings - Artist Name and Song Title
 		Gtk.ListStore musicListStore = new Gtk.ListStore (typeof (string), typeof (string));
@@ -64,5 +96,14 @@ public class TreeViewExample
  
 		// Show the window and everything on it
 		window.ShowAll ();
+	}
+
+	private void f1(TreeViewColumn col, CellRenderer cell, TreeModel model, TreeIter iter)
+	{
+		(cell as CR).Text = (string)model.GetValue(iter, 0);
+	}
+	private void f2(TreeViewColumn col, CellRenderer cell, TreeModel model, TreeIter iter)
+	{
+		(cell as CR).Text = (string)model.GetValue(iter, 1);
 	}
 }
