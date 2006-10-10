6efcd5117f899973ef5da033b5187bd190982fa1
ohad <ohad@cdcc5ea1-6e19-0410-9661-ca766f925b7c>
2006-10-10 15:59:15 +0000

    Some GUI simplifications
    git-svn-id: file:///svn/ttime@86 cdcc5ea1-6e19-0410-9661-ca766f925b7c

--- ruby_trunk/gui/main_window.rb
+++ ruby_trunk/gui/main_window.rb
@@ -2,14 +2,17 @@ require 'libglade2'
 
 class MainWindow
   def initialize
-    @glade = GladeXML.new("ttime.glade", "MainWindow") { |handler| method(handler) }
+    @glade = GladeXML.new("ttime.glade") { |handler| method(handler) }
 
     @main_window = @glade["MainWindow"]
   end
 
   def on_quit_activate
-    p 'bye'
     Gtk.main_quit
+  end
+
+  def on_about_activate
+    @glade["AboutDialog"].run
   end
 end
 
