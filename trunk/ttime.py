c4d3c2e5de81ffd67178fc6d0955e15f6cd8a9d1
ohad <ohad@cdcc5ea1-6e19-0410-9661-ca766f925b7c>
2007-03-26 19:51:54 +0000

    A robust, easy-to-use long-background-worker-with-pretty-progress mechanism
    git-svn-id: file:///svn/ttime@182 cdcc5ea1-6e19-0410-9661-ca766f925b7c

--- trunk/ttime.py
+++ trunk/ttime.py
@@ -14,35 +14,21 @@ import ttime.logic.data
 import ttime.gui.MainWindow
 import ttime.gui.ProgressDialog
 
-class DataThread(threading.Thread):
-    def __init__(self, callback):
-        threading.Thread.__init__(self)
-        self.callback = callback
-        self.proceed = True
-
-    def abort(self):
-        self.proceed = False
-
-    def run(self):
-        str = ttime.logic.data.repy_data()
-        if self.proceed: self.callback(str)
-
-# FIXME: This is EXTREMELY messy :(
-if __name__ == '__main__':
-    def start_main_window(throw_away_string):
+class MainWindowStarter:
+    def start_main_window(self):
         print "Data has been downloaded, starting main window"
-        stat_flag = False
-        progress.hide()
-        m = ttime.gui.MainWindow.MainWindow()
-    def stat_true():
-        stat()
-        return stat_flag
-    stat_flag = True
-    d = DataThread(start_main_window)
-    d.start()
-    progress = ttime.gui.ProgressDialog.ProgressDialog()
-    stat = progress.get_status_func()
-    stat('Downloading REPY data...')
-    gobject.timeout_add(100,stat_true)
+        self.main_window = ttime.gui.MainWindow.MainWindow()
 
-    gtk.main()
+    def get_data(self):
+        self.repy_data = ttime.logic.data.repy_data()
+
+    def __init__(self):
+        progress = ttime.gui.ProgressDialog.ProgressDialog(
+                'Loading REPY data', self.get_data,
+                callback_func = self.start_main_window
+                )
+
+        gtk.main()
+
+if __name__ == '__main__':
+    m = MainWindowStarter()
--- trunk/ttime/gui/ProgressDialog.py
+++ trunk/ttime/gui/ProgressDialog.py
@@ -1,20 +1,61 @@
+#!/usr/bin/env python
+
+"""Provides an interface to run a long operation in a background worker
+thread, while displaying an appropriate graphical hint"""
+
+import pygtk
+pygtk.require('2.0')
 import gtk
+import gobject
+gobject.threads_init()
+import threading
 
 # FIXME: Make this use gettext instead
 _ = str
 
+# FIXME: "Stop" button only calls the cancel callback, currently no good
+#        way to actually kill off a thread
+
+class ProgressDialogWorker(threading.Thread):
+    def __init__(self, worker_func, callback_func):
+        threading.Thread.__init__(self)
+        self.worker_func = worker_func
+        self.callback_func = callback_func
+
+    def run(self):
+        self.worker_func()
+        self.callback_func()
+
 class ProgressDialog(gtk.Dialog):
-    def __init__(self, stop_callback = None):
+    def __init__(self, label, worker_func, callback_func = None,
+            cancel_func = None):
+        """worker_func is run in a background thread. If cancel_func is
+        provided, then a 'stop' button is displayed, and cancel_func is
+        called if it is pressed or the window is closed. When the worker
+        thread finishes, the dialog is hidden, and if the stop button was
+        not pressed, callback_func - if provided - is called."""
+        # FIXME: The window should not be closable if cancel_func is not
+        # given
         gtk.Dialog.__init__(self)
-        self.set_title(_('Progress'))
+        self.set_title(label)
         self.set_modal(True)
 
-        self.label = gtk.Label('Label')
+        self.callback_func = callback_func
+
+        worker = ProgressDialogWorker(worker_func, self.worker_func_done)
+        worker.start()
+
+        self.cancelled = False
+        self.done = False
+
+        self.label = gtk.Label(label)
         self.progressbar = gtk.ProgressBar()
 
-        if stop_callback:
+        gobject.timeout_add(100, self.pulse)
+
+        if cancel_func:
             btn_stop = gtk.Button(stock = 'gtk-stop')
-            self.stop_callback = stop_callback
+            self.cancel_func = cancel_func
             btn_stop.connect('clicked', self.stop_clicked)
             self.connect('response', self.dlg_response)
             self.action_area.pack_end(btn_stop)
@@ -24,25 +65,22 @@ class ProgressDialog(gtk.Dialog):
 
         self.show_all()
 
+    def pulse(self):
+        self.progressbar.pulse()
+        return not self.done and not self.cancelled
+
+    def worker_func_done(self):
+        self.done = True
+        if not self.cancelled:
+            self.hide()
+            if self.callback_func: self.callback_func()
+
     def stop_clicked(self, obj):
-        self.stop_callback()
+        if self.cancel_func:
+            self.cancelled = True
+            self.cancel_func()
 
     def dlg_response(self, dialog, response_id):
-        self.stop_callback()
-
-    def get_status_func(self):
-        def status_func(label = None,fraction = None):
-            if label: self.label.set_label(label)
-            if fraction: self.progressbar.set_fraction(fraction)
-            else: self.progressbar.pulse()
-
-        return status_func
-
-# FIXME: Remove this
-def bla():
-    print 'click!'
-if __name__ == '__main__':
-    g = ProgressDialog(stop_callback = bla)
-    d = g.get_status_func()
-    d('bla',0.5)
-    gtk.main()
+        if self.cancel_func:
+            self.cancelled = True
+            self.cancel_func()
