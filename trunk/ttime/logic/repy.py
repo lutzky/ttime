851b187107229a347a66a00e31f6de86eec2b9d5
ohad <ohad@cdcc5ea1-6e19-0410-9661-ca766f925b7c>
2007-04-05 17:09:16 +0000

    Initial handling of sports courses
    git-svn-id: file:///svn/ttime@199 cdcc5ea1-6e19-0410-9661-ca766f925b7c

--- trunk/ttime/logic/repy.py
+++ trunk/ttime/logic/repy.py
@@ -17,24 +17,35 @@ COURSE_BANNER_REGEX = re.compile(u"""\+-----------------------------------------
 \| שעות הוראה בשבוע:ה-(\d) ת-(\d) +נק: (.*) *\|
 \+------------------------------------------\+""", re.UNICODE)
 
+SPORTS_BANNER_REGEX = re.compile(u"""\+===============================================================\+
+| +מקצועות ספורט -.*\|
+\+===============================================================\+""", re.UNICODE)
+
 # FIXME: Finish this
 def parse_repy_data():
     if prefs.options.do_parsing:
-        # Functional or just plain messed up? You be the judge.
-        parsed_faculties = [
-                parse_raw_faculty(*tuple(
-                    re.split(FACULTY_BANNER_REGEX, raw_faculty)[1:3]
-                    ))
-                for raw_faculty in data.repy_data().split("\n\n")
-                ]
+        non_sport_raw, sport_header, sport_raw = tuple(
+                re.split(SPORTS_BANNER_REGEX, data.repy_data())
+                )
 
-        # FIXME: Later non-standard faculties mess this up
+        # FIXME: We should do something about sport_raw too, shouldn't we? :)
 
-        print parsed_faculties
+        for name in parse_non_sport_data(non_sport_raw):
+            if name: print name
+        return None
 
-        return parsed_faculties
+def parse_non_sport_data(raw_data):
+    # Functional or just plain messed up? You be the judge.
+    parsed_faculties = [
+        parse_raw_faculty(
+            *tuple(re.split(FACULTY_BANNER_REGEX, raw_faculty)[1:3])
+            )
+        for raw_faculty in raw_data.split("\n\n")
+    ]
+    return parsed_faculties
 
-def parse_raw_faculty(name, raw_data):
-    print name
+
+def parse_raw_faculty(name = None, raw_data = None):
+    if not name:
+        return None
     return name
-
