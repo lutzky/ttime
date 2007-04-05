e243b29ddff8ec02fea353b31f4883bd18618022
ohad <ohad@cdcc5ea1-6e19-0410-9661-ca766f925b7c>
2007-04-05 16:28:25 +0000

    Added a handy-dandy REPY viewer
    git-svn-id: file:///svn/ttime@198 cdcc5ea1-6e19-0410-9661-ca766f925b7c

--- trunk/ttime.py
+++ trunk/ttime.py
@@ -3,6 +3,7 @@
 import os
 import sys
 
+import pango
 import pygtk
 pygtk.require('2.0')
 import gtk
@@ -32,14 +33,34 @@ class MainWindowStarter:
 	
         gtk.main()
 
+def show_parsed_repy():
+    w = gtk.Window()
+    w.set_default_size(600,600)
+    s = gtk.ScrolledWindow()
+    tb = gtk.TextBuffer()
+    tv = gtk.TextView(tb)
+    tv.modify_font(pango.FontDescription ("Mono"))
+    s.add(tv);
+    s.set_policy(gtk.POLICY_NEVER, gtk.POLICY_ALWAYS)
+    tb.set_text(logic.data.repy_data())
+    w.add(s)
+    w.show_all()
+    w.connect('destroy',gtk.main_quit)
+    gtk.main()
+
 if __name__ == '__main__':
     parser = OptionParser("usage: %prog { -c }")
     parser.add_option("-c", "--cache", dest="usecache", help="Use cached REPY",
             default = False, action="store_true")
     parser.add_option("-p", "--parse", dest="do_parsing", help="Actually parse",
             default = False, action="store_true")
+    parser.add_option("-v", "--view_repy", dest="viewrepy", help="Jusy show the REPY file",
+            default = False, action="store_true")
     (options,args) = parser.parse_args()
     prefs.options = options
 
-    m = MainWindowStarter()
+    if options.viewrepy:
+        show_parsed_repy()
+    else:
+        m = MainWindowStarter()
     print 'Letting all threads die...'
