234c329f9f60faebc0ba4a3433421303fbad462a
ohad <ohad@cdcc5ea1-6e19-0410-9661-ca766f925b7c>
2007-04-06 17:16:35 +0000

    State machine ARGH!!!!!
    git-svn-id: file:///svn/ttime@202 cdcc5ea1-6e19-0410-9661-ca766f925b7c

--- trunk/ttime/logic/repy.py
+++ trunk/ttime/logic/repy.py
@@ -21,7 +21,37 @@ SPORTS_BANNER_REGEX = re.compile(u"""\+=========================================
 | +מקצועות ספורט -.*\|
 \+===============================================================\+""", re.UNICODE)
 
+LECTURER_IN_CHARGE_REGEX = re.compile(u"\| מורה  אחראי :(.*?) *\|", re.UNICODE)
+FIRST_TEST_DATE_REGEX = re.compile(u"\| מועד ראשון :(.*?) *\|", re.UNICODE)
+SECOND_TEST_DATE_REGEX = re.compile(u"\| מועד שני   :(.*?) *\|", re.UNICODE)
+RISHUM_LINE_REGEX = re.compile(u"\|רישום +\|", re.UNICODE)
+EMPTY_LINE_REGEX = re.compile(u"\| +\|", re.UNICODE)
+
 COURSE_HEADER_REGEX = re.compile(u"""\n\|\s(\d\d\d\d\d\d) ([א-תףץךןם0-9()\/+#,.\-"' ]+?) *\|\n\| שעות הוראה בשבוע\:( *[א-ת].+?[0-9]+)+ +נק: (\d\.?\d) ?\|""", re.UNICODE)
+
+STATE_START, STATE_THING, STATE_DETAILS = range(3)
+
+# This is included due to Python not allowing assignments in if statements.
+# Thus, this ruby code
+#
+#   if m=/some_regex/.match(line)
+#     something = m[1]
+#   elsif m=/some_other_regex/.match(line)
+#     something_else = m[1]
+#
+# ...becomes this python code:
+#
+#   mre = Matcher() # once
+#
+#   if mre.match(some_regex, line):
+#       something = mre.groups[0]
+#   elif mre.match(other_regex, line):
+#       something_else = mre.groups[0]
+class Matcher:
+    def match(self,regex,str):
+        m = re.match(regex,str)
+        if m: self.groups = m.groups()
+        return m
 
 def single_space(str): return re.sub(' +',' ', str)
 
@@ -77,5 +107,26 @@ def parse_raw_course(header, body):
     # print course_id, course_name, academic_points, str(hours), '!'
 
     # TODO: The state-machine :/
+
+    current_lecture_group_number = 1
+
+    mre = Matcher()
+
+    state = STATE_START
+    for line in body.split("\n"):
+        if state == STATE_START:
+            if line[3] != '-':
+                if mre.match(LECTURER_IN_CHARGE_REGEX, line):
+                    lecturer_in_charge = single_space(mre.groups[0].strip())
+                elif mre.match(FIRST_TEST_DATE_REGEX, line):
+                    first_test_date = convert_test_date(mre.groups[0])
+                elif mre.match(SECOND_TEST_DATE_REGEX, line):
+                    second_test_date = convert_test_date(mre.groups[0])
+                elif RISHUM_LINE_REGEX.match(line) \
+                        or EMPTY_LINE_REGEX.match(line):
+                            state = STATE_THING
+                
+
+
 
     return (header, body)
