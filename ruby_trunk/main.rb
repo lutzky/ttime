5996663d802ae853499a0e3b7f579e96b51a3e36
Ohad Lutzky <ohad@lutzky.net>
2008-06-29 16:17:16 +0300

    Merged functionality of hebrew.sh, ttime-he and main-win32.rb into ordinary main.rb. (Run with --help)

--- ruby_trunk/main.rb
+++ ruby_trunk/main.rb
@@ -1,12 +1,25 @@
 #!/usr/bin/env ruby
 
 require 'pathname'
+require 'optparse'
 
 p = Pathname.new($0)
 
 if p.basename.to_s == 'main.rb'
   Dir.chdir p.parent.to_s
 end
+
+OptionParser.new do |opts|
+  opts.on("--hebrew", "Force Hebrew locale") do |env|
+    ENV["LC_ALL"]="he_IL.UTF-8"
+    ENV["LANG"]="he_IL.UTF-8"
+    ENV["LANGUAGE"]="he_IL.UTF-8"
+  end
+
+  opts.on("--win32-hack", "Hack for win32 compatibility") do |env|
+    ENV["HOME"] = Dir.pwd
+  end
+end.parse!
 
 require 'gui/main_window'
 
