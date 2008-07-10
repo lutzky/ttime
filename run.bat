ef791eea195b3b6f3ed7805ae57820c302bf3b25
Ohad Lutzky <ohad@hamster.(none)>
2008-07-10 22:02:09 +0300

    Reset to common trunk

--- run.bat
+++ run.bat
@@ -3,7 +3,8 @@ cd ruby\lib\GTK\bin
 pango-querymodules > ..\etc\pango\pango.modules
 gtk-query-immodules-2.0.exe > ..\etc\gtk-2.0\gtk.immodules
 gdk-pixbuf-query-loaders > ..\etc\gtk-2.0\gdk-pixbuf.loaders
-cd ..\..\..\..\ruby_trunk
+cd ..\..\..\..\
 del data\technion.mrshl
-..\ruby\bin\ruby ttime --hebrew --win32-hack
+cd bin
+..\ruby\bin\ruby ttime --win32-hack
 cd ..
