a4b750fb461b15dcad172b923c86496464cef5de
Ohad Lutzky <ohad@lutzky.net>
2008-08-27 13:21:11 +0300

    Minimal running tweaks

--- lib/ttime/ratings/minimal_running.rb
+++ lib/ttime/ratings/minimal_running.rb
@@ -8,17 +8,31 @@ module TTime
       settings_name :minimal_running
       default_settings :enabled => false
 
+      def weight
+        1
+      end
+
       def rate_schedule
         return 0 unless self.enabled
 
         rating = 0
         event_list.each do |ev|
           each_following_event(ev) do |follower|
-            rating -= 1 unless event_building(ev) == event_building(follower)
+            rating -= self.distance(ev, follower)
           end
         end
 
         rating
+      end
+
+      def distance(ev, follower)
+        if event_building(ev) != event_building(follower)
+          return 2 # TODO better metric for this
+        elsif ev.place.rstrip == "" or follower.place.rstrip == ""
+          return 1 # No data, could be different places, don't know how bad
+        elsif ev.place == follower.place
+          return 0 # Exact same place
+        end
       end
 
       def name
